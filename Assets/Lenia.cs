84c63d189434a67b5000e75c8e718ad0ec43dba8
Anthony <cerciellothony@gmail.com>
2024-03-19 11:19:46 +0100

    fix kernel normalization

--- Assets/Lenia.cs
+++ Assets/Lenia.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
-public class Compute : MonoBehaviour
+public class Lenia : MonoBehaviour
 {
+    [SerializeField] private float timestep = 0.05f;
+    [SerializeField] private int kernelSize = 12;
     [SerializeField] private Gradient colorGradient;
-    [SerializeField] private RawImage img;
     [SerializeField] private ComputeShader cs;
 
     public Texture2D gradient;
@@ -15,34 +18,40 @@ public class Compute : MonoBehaviour
     private int initKernelID;
     private int drawKernelID;
     private int mainKernelID;
+    private int drawKernelKernelID;
 
-    private RenderTexture result;
-    private RenderTexture cells;
-    private RenderTexture cellsBuffer;
+    public RenderTexture result;
+    public RenderTexture cells;
+    public RenderTexture cellsBuffer;
+    public RenderTexture kernel;
     
     void Start()
     {
         initKernelID = cs.FindKernel("Init");
         drawKernelID = cs.FindKernel("Draw");
         mainKernelID = cs.FindKernel("Main");
+        drawKernelKernelID = cs.FindKernel("DrawKernel");
         
-        cells = RenderTexture.GetTemporary(1920, 1080);
+        kernel = RenderTexture.GetTemporary(kernelSize, kernelSize);
+        kernel.enableRandomWrite = true;
+        
+        cells = RenderTexture.GetTemporary(Screen.width, Screen.height);
         cells.enableRandomWrite = true;
-        cells.filterMode = FilterMode.Bilinear;
         cells.wrapMode = TextureWrapMode.Repeat;
         
-        cellsBuffer = RenderTexture.GetTemporary(1920, 1080);
-        cellsBuffer.filterMode = FilterMode.Bilinear;
+        cellsBuffer = RenderTexture.GetTemporary(Screen.width, Screen.height);
         cellsBuffer.wrapMode = TextureWrapMode.Repeat;
         
-        result = RenderTexture.GetTemporary(1920, 1080);
+        result = RenderTexture.GetTemporary(Screen.width, Screen.height);
         result.enableRandomWrite = true;
         result.wrapMode = TextureWrapMode.Repeat;
         
-        cs.SetTexture(initKernelID, "Result", result);
-        cs.Dispatch(initKernelID, GetThreadGroupSize().x, GetThreadGroupSize().y, 1);
+        cs.SetFloat("Timestep", timestep);
+        cs.SetInt("Width", Screen.width);
+        cs.SetInt("Height", Screen.height);
         
-        img.texture = result;
+        cs.SetTexture(initKernelID, "Cells", cells);
+        cs.Dispatch(initKernelID, GetThreadGroupSize().x, GetThreadGroupSize().y, 1);
         
         GenerateGradientLookupTexture();
         StartCoroutine(Step());
@@ -64,21 +73,34 @@ public class Compute : MonoBehaviour
 
     private void Update()
     {
+        ReallocateResources();
         Draw();
     }
 
+    private void ReallocateResources()
+    {
+        if (kernel.width != kernelSize || kernel.height != kernelSize)
+        {
+            RenderTexture.ReleaseTemporary(kernel);
+            kernel = RenderTexture.GetTemporary(kernelSize, kernelSize);
+            kernel.enableRandomWrite = true;
+        }
+    }
+    
     private void Draw()
     {
+        cs.SetInt("KernelSize", kernelSize);
+        cs.SetTexture(drawKernelKernelID, "Result", kernel);
+        cs.Dispatch(drawKernelKernelID, Mathf.CeilToInt(kernelSize / 8f), Mathf.CeilToInt(kernelSize / 8f), 1);
+        
         if (!Input.GetKey(KeyCode.Mouse0)) return;
-        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(img.rectTransform, Input.mousePosition, null,
-                out Vector2 position)) return;
         
         Graphics.CopyTexture(cells, cellsBuffer);
         cs.SetTexture(drawKernelID, "Result", result);
         cs.SetTexture(drawKernelID, "Cells", cells);
         cs.SetTexture(drawKernelID, "CellsBuffer", cellsBuffer);
         cs.SetTexture(drawKernelID, "Gradient", gradient);
-        cs.SetVector("PointerPosition", position);
+        cs.SetVector("PointerPosition", Input.mousePosition);
         cs.Dispatch(drawKernelID, GetThreadGroupSize().x, GetThreadGroupSize().y, 1);
     }
 
@@ -92,7 +114,7 @@ public class Compute : MonoBehaviour
             cs.SetTexture(mainKernelID, "CellsBuffer", cellsBuffer);
             cs.SetTexture(mainKernelID, "Gradient", gradient);
             cs.Dispatch(mainKernelID, GetThreadGroupSize().x, GetThreadGroupSize().y, 1);
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(timestep);
         }
     }
 
@@ -107,12 +129,15 @@ public class Compute : MonoBehaviour
         RenderTexture.ReleaseTemporary(result);
         RenderTexture.ReleaseTemporary(cells);
         RenderTexture.ReleaseTemporary(cellsBuffer);
+        RenderTexture.ReleaseTemporary(kernel);
     }
 
     private void OnGUI()
     {
-        GUI.DrawTexture(new Rect(0, 0, 192, 108), result);
-        GUI.DrawTexture(new Rect(192, 0, 192, 108), cells);
-        GUI.DrawTexture(new Rect(384, 0, 192, 108), cellsBuffer);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), result);
+        
+        GUI.DrawTexture(new Rect(0, 0, 192, 108), cells);
+        GUI.DrawTexture(new Rect(192, 0, 192, 108), cellsBuffer);
+        GUI.DrawTexture(new Rect(384, 0, 128, 128), kernel);
     }
 }
